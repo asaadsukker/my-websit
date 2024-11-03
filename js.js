9446f54f30d92a40678cf15f924e493db56000ad
asaadsukker <ad3683010@gmail.com>
2024-11-03 03:28:29 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+var a =document.getElementById("A");
+function aa(){
+    let x=window.prompt("Hello student input password in box");
+    if(x=="asad1234"){
+        window.open("class.html");
+    }
+}
+a.onclick=aa;
+var b =document.getElementById("B");
+function bb(){
+    let y=window.prompt("Hello T.mohammad input password in box");
+    if(y=="asad1234"){
+        window.open("students-book.pdf");
+}}
+b.onclick=bb;
+var d =document.getElementById("D");
+function dd(){
+    let z=window.prompt("Hello!input password in box");
+}
+d.onclick=dd;
